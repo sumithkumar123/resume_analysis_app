d4da63989138bf44bcd4aaf9799bf9ff9f355865
sumithkumar123 <147124944+sumithkumar123@users.noreply.github.com>
2025-02-18 12:27:57 +0530

    Update auth.js

--- resume-analysis-app/routes/auth.js
+++ resume-analysis-app/routes/auth.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-require('dotenv').config(); // Load .env in this file too
-
+require('dotenv').config(); 
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -10,7 +9,6 @@ router.post('/login', (req, res) => {
         return res.status(400).json({ error: 'Missing username or password' });
     }
 
-    // Hardcoded credentials (for this assignment ONLY)
     if (username === 'naval.ravikant' && password === '05111974') {
         const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.json({ JWT: token });
